e04200fdb4c4bc96caf16a116faaea6519c4cbe8
Santiago Ramos <santiagodotsh@gmail.com>
2024-09-29 12:49:07 -0500

    Config NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function HomeScreen() {
+  return (
+    <View className='mt-10'>
+      <Text className='text-3xl font-bold text-blue-500'>
+        Expo Navigation
+      </Text>
+    </View>
+  )
+}
